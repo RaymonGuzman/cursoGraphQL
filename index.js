eb53a1f437cc5ceab9ac81f8f17f5e8832419cd4
Raymon Fco. Guzmán Domínguez <frankguzman23@gmail.com>
2020-11-17 12:15:49 -0400

    Identando código y agregando context

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const { ApolloServer } = require('apollo-server');
-const  typeDefs  = require('./schema');
-const  resolvers  = require('./resolvers');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 
 
 //servidor
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: () => {
+        const context = "Context";
+        return { context }
+    }
 
 });
 
 
 //arrancar el servidir
-server.listen().then ( ({url}) => {
+server.listen().then(({ url }) => {
     console.log(`Servidor corriendo en la URL ${url}`);
 })
--- schema.js
+++ schema.js
@@ -1,21 +1,21 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 //Schema
 
 const typeDefs = gql`
-    type Curso {
-        titulo: String
-        tecnologia: String
-    }
-    input tecnologiaInput{
-        tecnologia: String
-    }
-    # type Tecnologia {
-    #     tecnologia: String
-    # }
-    type Query { 
-        obtenerCursos(input:tecnologiaInput) : [Curso]
-        # obtenerTecnologia: [Tecnologia]
-    }
+  type Curso {
+    titulo: String
+    tecnologia: String
+  }
+  input tecnologiaInput {
+    tecnologia: String
+  }
+  # type Tecnologia {
+  #     tecnologia: String
+  # }
+  type Query {
+    obtenerCursos(input: tecnologiaInput!): [Curso]
+    # obtenerTecnologia: [Tecnologia]
+  }
 `;
 
 module.exports = typeDefs;
