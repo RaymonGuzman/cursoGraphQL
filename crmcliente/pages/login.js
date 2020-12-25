d64c5cd051d093d5f781c0568906ac1c79fa7644
Raymon Fco. Guzmán Domínguez <frankguzman23@gmail.com>
2020-12-25 03:48:31 -0400

    S14:67 S15:68 - Agregando condicion para mostrar layout diferente y Creando formulario para Login ccon Next y Tailwind

---
+++ crmcliente/pages/login.js
@@ -0,0 +1,55 @@
+import React, { Fragment } from 'react';
+import Layout from '../components/Layout';
+
+const login = () => {
+  return (
+    <div>
+      <Layout>
+        <h1 className="text-2xl text-center text-white font-light"> Login </h1>
+
+        <div className="flex justify-center mt-5">
+          <div className="w-full max-w-sm">
+            <form className="bg-white shadow-md px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Email Usuario"
+                ></input>
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Password Usuario"
+                ></input>
+              </div>
+
+              <input
+                type="submit"
+                className="bg-gray-800 w-full mt-5 text-white uppercase hover:bg-gray-600"
+                value="Iniciar Sesión"
+              ></input>
+            </form>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
+};
+
+export default login;
---
+++ crmcliente/pages/nuevacuenta.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Layout from '../components/Layout';
+
+const nuevaCuenta = () => {
+  return (
+    <div>
+      <Layout>
+        <h1 className="text-2xl text-center text-white font-light"> Nueva Cuenta </h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default nuevaCuenta;
