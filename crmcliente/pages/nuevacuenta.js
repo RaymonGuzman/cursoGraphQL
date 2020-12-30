cce9b8437552980925ba0dcec39d68b883e30b38
Raymon Fco. Guzmán Domínguez <frankguzman23@gmail.com>
2020-12-29 20:42:55 -0400

    S15:76,77 - Creando un Usuario desde el formulario y mostrar mensaje de error si ya esta registrado

--- crmcliente/pages/nuevacuenta.js
+++ crmcliente/pages/nuevacuenta.js
@@ -1,28 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Layout from '../components/Layout';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { useQuery, gql } from '@apollo/client';
+import { useMutation, gql } from '@apollo/client';
 
-const QUERY= gql`  {
-  obtenerProductos{
-    id
-    nombre
-    existencia
-    precio
-    creado
+const NUEVA_CUENTA = gql`
+  mutation nuevoUsuario($input: UsuarioInput) {
+    nuevoUsuario(input: $input) {
+      nombre
+      apellido
+      email
+      creado
+    }
   }
-}
 `;
 
 const nuevaCuenta = () => {
+  //State para guardar mensaje
+  const [mensaje, guardarMensaje] = useState(null);
 
-  //Realizando consulta de prueba
-
-  const { data, loading, error } = useQuery(QUERY);
-  console.log(loading);
-  console.log(data);
-  console.log(error);
+  //Mutation para crear nueva usuario
+  const [nuevoUsuario] = useMutation(NUEVA_CUENTA);
 
   // Validación del formulario
   const formik = useFormik({
@@ -34,23 +32,55 @@ const nuevaCuenta = () => {
     },
     validationSchema: Yup.object({
       nombre: Yup.string()
-                    .required('El Nombre es obligatorio'),
-      apellido: Yup.string().required('El Apellido es obligatorio'),
+                  .required('El Nombre es obligatorio'),
+      apellido: Yup.string()
+                  .required('El Apellido es obligatorio'),
       email: Yup.string()
-                    .email('El email no es válido')
-                    .required('El Email es obligatorio'),
+                  .email('El email no es válido')
+                  .required('El Email es obligatorio'),
       password: Yup.string()
-                    .required('El Password es obligatorio')
-                    .min(6, 'El Password debe al menos tener 6 caracteres'),
+                  .required('El Password es obligatorio')
+                  .min(6, 'El Password debe al menos tener 6 caracteres'),
     }),
-    onSubmit: (valores) => {
-      console.log(valores);
+    onSubmit: async (valores) => {
+      // console.log(valores);
+      const { nombre, apellido, email, password } = valores;
+
+      try {
+        const { data } = await nuevoUsuario({
+          variables: {
+            input: {
+              nombre,
+              apellido,
+              email,
+              password,
+            },
+          },
+        });
+
+        console.log(data);
+      } catch (error) {
+        // console.log(error.message);
+        guardarMensaje(error.message);
+
+        setTimeout(() => {
+          guardarMensaje(null);
+        }, 3000);
+      }
     },
   });
 
+  const monstrarMensaje = () => {
+    return (
+      <div className="bg-white py-2 px-3 w-full my-3 max-w-sm text-center text-red-600 mx-auto">
+        <p> {mensaje} </p>
+      </div>
+    );
+  };
   return (
     <div>
       <Layout>
+        {mensaje && monstrarMensaje()}
         <h1 className="text-2xl text-center text-white font-light"> Login </h1>
 
         <div className="flex justify-center mt-5">
