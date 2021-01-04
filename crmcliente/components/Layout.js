76c79e6bf39b41cab150fc568d6240a920b5fccd
Raymon Fco. Guzmán Domínguez <frankguzman23@gmail.com>
2021-01-03 21:23:39 -0400

    S16:83 - Mostrando los clientes por Usuario Logueado

--- crmcliente/components/Layout.js
+++ crmcliente/components/Layout.js
@@ -29,7 +29,7 @@ const layout = ({ children }) => {
         <div className="bg-gray-200 min-h-screen">
           <div className="flex min-h-screen">
             <Sidebar />
-            <main className="sm:w2/3 xl:4/5 sm:min-h-screen p-5">
+            <main className="sm:w-2/3 xl:4/5 sm:min-h-screen p-5">
               {children}
             </main>
           </div>
--- crmcliente/pages/index.js
+++ crmcliente/pages/index.js
@@ -1,9 +1,52 @@
 import Layout from '../components/Layout';
+import { useQuery, gql } from '@apollo/client';
+
+const CLIENTES_VENDEDOR = gql`
+  {
+    obtenerClienteVendedor {
+      id
+      nombre
+      apellido
+      empresa
+      email
+      vendedor
+    }
+  }
+`;
 
 export default function Home() {
+  const { data, loading, error } = useQuery(CLIENTES_VENDEDOR);
+
+  if (loading) return 'Cargando...';
+
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light"> Clientes </h1>
+
+      <table className="table-auto shadow-md mt-10 w-full w-lg">
+        <thead className="bg-gray-800 text-white">
+          <tr>
+            <th className="w-1/3 py-2">Nombre</th>
+            <th className="w-2/3 py-2">Empresa</th>
+            <th className="w-1/3 py-2">Email</th>
+          </tr>
+        </thead>
+        <tbody className="bg-white">
+          {data.obtenerClienteVendedor.map((cliente) => (
+            <tr key={cliente.id}>
+              <td className="border px-4 py-2 border-gray-300">
+                {cliente.nombre} {cliente.apellido}
+              </td>
+              <td className="border px-4 py-2 border-gray-300">
+                {cliente.empresa}
+              </td>
+              <td className="border px-4 py-2 border-gray-300">
+                {cliente.email}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
