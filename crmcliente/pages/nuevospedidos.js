d93d83d5970014f82e81efed994ea19b7469f2b8
Raymon Fco. Guzmán Domínguez <frankguzman23@gmail.com>
2021-02-14 18:06:45 -0400

    Creando la pagina para nuevos Pedidos

---
+++ crmcliente/pages/nuevospedidos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const nuevospedidos = () => {
+  return (
+    <Layout>
+      <h1>Nuevos Pedidos</h1>
+    </Layout>
+  );
+};
+
+export default nuevospedidos;
--- crmcliente/pages/pedidos.js
+++ crmcliente/pages/pedidos.js
@@ -1,10 +1,18 @@
 import React, { Fragment } from 'react';
 import Layout from '../components/Layout';
+import Link from 'next/link';
+
 const Pedidos = () => {
   return (
     <div>
       <Layout>
-        <h1 className="text-2xl text-gray-800 font-light"> Pedidos </h1>
+        <h1 className="text-2xl text-gray-800"> Pedidos </h1>
+
+        <Link href="/nuevospedidos">
+          <a className="bg-gray-800 text-white px-3 py-2 mb-3 inline-block rounded font-bold text-sm shadow-md hover:bg-blue-500">
+            Nuevos Pedidos
+          </a>
+        </Link>
       </Layout>
     </div>
   );
