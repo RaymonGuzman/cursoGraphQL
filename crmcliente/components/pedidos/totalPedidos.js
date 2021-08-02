56890b32c239cc4971747f8c3e48f0461c7465ed
Raymon Fco. Guzmán Domínguez <frankguzman23@gmail.com>
2021-08-01 23:49:51 -0400

    Dando estilo al componente que muestra el total del pedido

--- crmcliente/components/pedidos/totalPedidos.js
+++ crmcliente/components/pedidos/totalPedidos.js
@@ -7,10 +7,10 @@ const totalPedidos = () => {
   const context = useContext(PedidoContext);
   const { total } = context;
   return (
-    <>
-      <h4>Total</h4>
-      <p>{total}</p>
-    </>
+    <div className="flex mt-5 justify-between bg-white p-3">
+      <h2 className="text-gray-800 text-lg">Total</h2>
+      <p className="text-gray-800 mt-1">$ {total}</p>
+    </div>
 
   );
 }
