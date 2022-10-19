75a2c20082bcad8185924c6033f17d49f8b57ab0
Raymon Francisco Guzmán Domínguez <ray@pop-os.localdomain>
2022-10-19 00:01:10 -0400

    S22:133 Agregando validación para los pedidos

--- crmcliente/pages/nuevospedidos.js
+++ crmcliente/pages/nuevospedidos.js
@@ -8,21 +8,27 @@ import TotalPedidos from '../components/pedidos/totalPedidos';
 
 const nuevospedidos = () => {
   const context = useContext(PedidoContext);
-  const { producto, total, cliente } = context;
+  const { productos, total, cliente } = context;
+
+  console.log(Object.keys(cliente).length);
+  const validarPedido = () => {
+    return !productos.every(producto => producto.cantidad > 0) || total === 0 || Object.keys(cliente).length === 0 ? " opacity-50 cursor-not-allowed " : "";
+  }
+
   return (
     <Layout>
       <h1>Nuevos Pedidos</h1>
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-lg">
-          <AsignarCliente/>
-          <AsignarProducto/>
+          <AsignarCliente />
+          <AsignarProducto />
           <ListarProductos />
           <TotalPedidos />
           <input
-                type="button"
-                className="w-full bg-gray-800 mt-5 py-2 text-white uppercase hover:bg-gray-600"
-                value="Registrar Pedido"
-              />
+            type="button"
+            className={`w-full bg-gray-800 mt-5 py-2 text-white uppercase hover:bg-gray-600 ${validarPedido()}`}
+            value="Registrar Pedido"
+          />
         </div>
       </div>
     </Layout>
