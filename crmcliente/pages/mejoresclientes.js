d7c4202b4e985181c64c260fb50579dd7dccdb7b
Raymon Francisco Guzmán Domínguez <frankguzman23@gmail.com>
2022-12-09 00:54:35 -0400

    S24:147 Graficando los Mejores Clientes

--- crmcliente/pages/mejoresclientes.js
+++ crmcliente/pages/mejoresclientes.js
@@ -1,10 +1,74 @@
-// import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../components/Layout';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { gql, useQuery } from '@apollo/client';
+
+const MEJORES_CLIENTES = gql`
+    query mejoresClientes{
+        obtenerMejoresClientes{
+        cliente{
+            nombre
+            apellido
+            empresa
+            email
+            telefono
+        }
+        total
+        }
+    }
+`;
 
 const MejoresClientes = () => {
+
+    const { data, loading, error, startPolling, stopPolling } = useQuery(MEJORES_CLIENTES);
+
+    useEffect( () => {
+        startPolling(1000);
+        return () => {
+            stopPolling();
+        }
+
+    },[startPolling, stopPolling])
+
+    console.log(loading);
+
+    if (loading) return 'Cargando...'
+    console.log(data);
+
+    const mejoresClientes = [];
+
+    data.obtenerMejoresClientes.map((cliente, index) => {
+        mejoresClientes[index] = { ...cliente.cliente[0], nombre:cliente.cliente[0].nombre+' '+cliente.cliente[0].apellido, total: cliente.total }
+    });
+
+    console.log(mejoresClientes);
+
     return (
         <Layout>
             <h1 className="text-2xl text-gray-800"> Mejores Clientes </h1>
+
+            <ResponsiveContainer width="80%" height="60%">
+                <BarChart
+                    className="mt-10"
+                    // width={500}
+                    // height={300}
+                    data={mejoresClientes}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="nombre" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="total" fill="#3182CE" />
+                    {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
+                </BarChart>
+            </ResponsiveContainer>
         </Layout>
     );
 }
--- crmcliente/pages/mejoresvendedores.js
+++ crmcliente/pages/mejoresvendedores.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../components/Layout';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { gql, useQuery } from '@apollo/client';
@@ -16,56 +16,10 @@ const MEJORES_VENDEDORES = gql`
     }
 `;
 
-// const data = [
-//   {
-//     name: 'Page A',
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400,
-//   },
-//   {
-//     name: 'Page B',
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210,
-//   },
-//   {
-//     name: 'Page C',
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290,
-//   },
-//   {
-//     name: 'Page D',
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000,
-//   },
-//   {
-//     name: 'Page E',
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181,
-//   },
-//   {
-//     name: 'Page F',
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500,
-//   },
-//   {
-//     name: 'Page G',
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100,
-//   },
-// ];
-
 
 const MejoresVendedores = () => {
 
     const { data, loading, error, startPolling, stopPolling } = useQuery(MEJORES_VENDEDORES);
-    // console.log(data);
 
     useEffect( () => {
         startPolling(1000);
@@ -79,13 +33,10 @@ const MejoresVendedores = () => {
 
     if (loading) return 'Cargando...'
     console.log(data);
-    // console.log(error);
 
     const mejoresVendedores = [];
 
     data.obtenerMejoresVendedores.map((vendedores, index) => {
-        // console.log(vendedores, index);
-        // console.log(vendedores.total);
         mejoresVendedores[index] = { ...vendedores.vendedor[0], total: vendedores.total }
     });
 
