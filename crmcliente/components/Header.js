8c1a12fa67421388831b6b8269d7237d5b246759
Raymon Francisco Guzmán Domínguez <frankguzman23@gmail.com>
2023-05-21 02:40:16 -0400

    Modificando nombre del query y agregando cambios para hacer el componente responsive

--- crmcliente/components/Header.js
+++ crmcliente/components/Header.js
@@ -4,11 +4,12 @@ import { useRouter } from 'next/router';
 
 const OBTENER_USUARIO = gql`
   {
-    obtenerUsuario {
+    obtenerUsuarioAutenticado {
       id
       nombre
       apellido
       email
+      rol
     }
   }
 `;
@@ -19,11 +20,11 @@ const Header = () => {
 
   if (loading) return <p>Loading...</p>;
 
-  if (!data?.obtenerUsuario) {
+  if (!data?.obtenerUsuarioAutenticado) {
     router.push('/login');
     return <p>Cargado...</p>;
   }
-  const { nombre, apellido } = data.obtenerUsuario;
+  const { nombre, apellido, rol } = data.obtenerUsuarioAutenticado;
 
   const cerrarSesion = () => {
     localStorage.removeItem('token');
@@ -32,18 +33,26 @@ const Header = () => {
   };
 
   return (
-    <div className="flex justify-between mb-6">
-      <p className="mr-2">
-        Hola {nombre} {apellido}
-      </p>
-      <button
-        className="bg-gray-800 text-white px-4 py-2 rounded font-bold text-sm shadow-md hover:bg-blue-500"
-        type="button"
-        onClick={() => cerrarSesion()}
-      >
-        Cerrar Sesión
-      </button>
-    </div>
+    <>
+      <div className="sm:flex sm:justify-between mb-0">
+        <p className="mr-25 mb-0 lg:mb-0">
+          Hola {nombre} {apellido}
+        </p>
+        <button
+          className="bg-gray-800 w-full sm:w-auto text-white px-4 py-2 rounded font-bold text-sm shadow-md hover:bg-blue-500"
+          type="button"
+          onClick={() => cerrarSesion()}
+          >
+          Cerrar Sesión
+        </button>
+
+      </div>
+      <div className="mb-6 mt-0">
+        <p>
+          {rol=== 'admin' ? 'Administrador(a)' : 'Vendedor(a)'}
+        </p>
+      </div>
+    </>
   );
 };
 
